00d6112a7a359bfef4de356131350a32b0caff1a
KylerianHD <anders.jon@web.de>
2024-04-04 13:40:28 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+namespace SteamWishlistJSONToMarkdownBlazor
+{
+    internal static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            ApplicationConfiguration.Initialize();
+            Application.Run(new Form1());
+        }
+    }
+}
